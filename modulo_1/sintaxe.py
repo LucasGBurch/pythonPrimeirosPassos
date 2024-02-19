038719f8c2fae387dc91ae47f9ad83877cc0500d
Lucas Burch <lucasburch@hotmail.com>
2024-02-19 10:47:22 -0300

    Sintaxe e variáveis
    Primeiras três aulas básicas do curso e anotações pertinentes nos comentários.

---
+++ modulo_1/sintaxe.py
@@ -0,0 +1,13 @@
+# Comentário de uma linha
+
+""" Título do comentário de múltiplas linhas
+Primeira linha
+Comentários de múltipla linha
+Terceira linha
+\ serve para quebrar linha
+"""
+
+nome = "Lucas" \
+" Burch" # \ Mesma coisa que +, só que permite a quebra de linha
+
+print(nome)
---
+++ modulo_1/variaveis.py
@@ -0,0 +1,8 @@
+nome_completo = "Lucas Burch"
+# Tipagem dinâmica. Não precisa declarar nem o tipo, como nas outras linguagens backend, nem const/let como no JS/TS
+# Snake_Case é um estilo comum em python para declarar as variáveis, funções e métodos
+# Para classes, é comum para o camelCase
+
+print(nome_completo)
+
+# Documentação das boas práticas na sintaxe de python: PEP 8
