3600e7e51c544eeb6e1083c7dff0adf8f3888602
Lucas Burch <lucasburch@hotmail.com>
2024-02-21 08:58:07 -0300

    Aula sobre Condicionais

---
+++ modulo_1/condicionais.py
@@ -0,0 +1,18 @@
+# if, elif e else
+# Exemplo de "if"
+
+idade = 17
+print("Exemplo de comando if")
+
+if idade <= 17:
+  print("É menor de idade")
+elif idade >= 18:
+  print("É maior de idade")
+
+if idade == 19:
+  print("Tem 19 anos")
+else:
+  print("Não tem 19 anos")
+
+if idade != 10:
+  print("Idade diferente de 10 anos")
